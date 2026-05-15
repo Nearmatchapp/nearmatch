0c58b542f746f9f9cbc82300b0b20d523b057959
Nearmatchapp <peterbecsky@gmail.com>
2026-05-15 13:57:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    include: /\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.jsx': 'jsx',
+      },
+    },
+  },
   build: {
     chunkSizeWarningLimit: 10000,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
   }
 })
