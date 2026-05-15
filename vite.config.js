9efd08168dadb3ffbe60c354daab1582f46303c8
Nearmatchapp <peterbecsky@gmail.com>
2026-05-15 23:01:49 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    include: /\.[jt]sx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-        '.jsx': 'jsx',
-      },
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 10000,
-  }
 })
