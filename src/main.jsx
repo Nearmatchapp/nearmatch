749920ea18f6184d992759399adda14566349208
Nearmatchapp <peterbecsky@gmail.com>
2026-05-19 22:03:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Admin from "./Admin.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const isAdmin = window.location.pathname === "/admin";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  isAdmin ? <Admin /> : <App />
+);
